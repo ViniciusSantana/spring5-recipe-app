4d0e3f46b3a641b69f4499bea17e86f7c86ff8de
Vinicius <viniciusasantana@gmail.com>
2020-03-21 18:08:06 -0300

    Use spring repository query syntax

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,13 +1,31 @@
 package guru.springframework.controllers;
 
+import guru.springframework.domain.Category;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnityOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
+
 @Controller
 public class IndexController {
+    private final CategoryRepository categoryRepository;
+    private final UnityOfMeasureRepository unityOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnityOfMeasureRepository unityOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unityOfMeasureRepository = unityOfMeasureRepository;
+    }
+
+
     @RequestMapping({"", "/", "/index"})
-    public String getIndexPage(){
-        System.out.println("print 123");
-        return  "index";
+    public String getIndexPage() {
+        Optional<Category> optionalCategory = this.categoryRepository.findByDescription("American");
+        var teaspoon = this.unityOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Cat id is : " + optionalCategory.get().getId());
+        System.out.println("UOM id is : " + teaspoon.get().getId());
+        return "index";
     }
 }
--- src/main/java/guru/springframework/repositories/UnityOfMeasureRepository.java
+++ src/main/java/guru/springframework/repositories/UnityOfMeasureRepository.java
@@ -3,5 +3,9 @@ package guru.springframework.repositories;
 import guru.springframework.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnityOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    public Optional<UnitOfMeasure> findByDescription(String description);
 }
